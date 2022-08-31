2918049a4eba6dca2b06b2989b8a61015dbbb7da
hozaifaziaqazi <102088128+hozaifaziaqazi@users.noreply.github.com>
2022-08-30 22:21:40 -0700

    Add files via upload

---
+++ texteditor/texteditor/see.py
@@ -0,0 +1,71 @@
+# I make this file - Hozaifa
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def main(request):
+    return render(request, 'main.html')
+
+
+def analyze(request):
+    djtext = request.POST.get('text', 'default')
+
+    # Check checkbox values
+    removepunc = request.POST.get('removepunc', 'off')
+    fullcaps = request.POST.get('fullcaps', 'off')
+    lowercaps = request.POST.get('lowercaps', 'off')
+    newlineremover = request.POST.get('newlineremover', 'off')
+    extraspaceremover = request.POST.get('extraspaceremover', 'off')
+
+    # Check which checkbox is on
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if fullcaps == "on":
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+
+        params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if lowercaps == "on":
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.lower()
+
+        params = {'purpose': 'Changed to Lowercase', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if extraspaceremover == "on":
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not (djtext[index] == " " and djtext[index + 1] == " "):
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if newlineremover == "on":
+        analyzed = ""
+        for char in djtext:
+            if char != "\n" and char != "\r":
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+
+    if removepunc != "on" and newlineremover != "on" and extraspaceremover != "on" and fullcaps != "on":
+        return HttpResponse("please select any operation and try again")
+
+    return render(request, 'analyze.html', params)
